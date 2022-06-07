289d8e35ff0e892b3283f90a6bbfac41654696dc
Jose Luis <joseluisgarcia567@gmail.com>
2022-06-07 17:46:49 +0200

    fixed .env

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
--- src/services/translations/deepLTranslationService.ts
+++ src/services/translations/deepLTranslationService.ts
@@ -3,11 +3,10 @@ import * as deepl from 'deepl-node';
 import { ITranslationService } from "./ITranslationService";
 
 export class DeepLTranslationService implements ITranslationService {
-    private authKey = process.env.DEEPL_AUTH_KEY;
     private translator: deepl.Translator;
 
     constructor() {
-        this.translator = new deepl.Translator(this.authKey!);
+        this.translator = new deepl.Translator(process.env.DEEPL_AUTH_KEY!);
     }
 
     public async translate(text: string, targetLang: string): Promise<string> {
